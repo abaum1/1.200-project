9535a301b76a9061dc9d05138f82e5e78b76e9b8
amelia-b <amelia_b@mit.edu>
2024-05-06 16:12:25 -0400

    modularize evaluation fn

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 # from 1_200_project import settings
 from q_learning.environment import Environment
-from q_learning.helpers import generate_state, filter_for_valid_actions
+from q_learning.helpers import generate_state, filter_for_valid_actions, save_q_table, load_q_table
 from itertools import product
 import settings
 import state_probabilities
@@ -8,10 +8,14 @@ import numpy as np
 from collections import deque
 import pandas as pd
 from typing import List, Tuple
+from q_learning.testing import run_evaluation
 
 
 def run_model(all_states: List[Tuple[int]], all_actions: List[Tuple[int]],
-              environment) -> pd.DataFrame:
+              discount_factor, learning_rt, explore_rt):
+
+    environment = Environment(len(all_states), len(all_actions), learning_rt,
+                              discount_factor, explore_rt)
 
     last_100_rewards = deque(maxlen=100)
     results_by_episode = []
@@ -28,8 +32,7 @@ def run_model(all_states: List[Tuple[int]], all_actions: List[Tuple[int]],
                 all_actions,
                 state)  # which actions can you take from a given state
             action_idx, action = environment.choose_action(
-                state_idx, possible_action_indices,
-                all_actions)  # filter for valid actions only
+                state_idx, possible_action_indices, all_actions)
             next_state, reward, done = environment.step(state, action)
             last_100_rewards.append(reward)
             next_state_idx = all_states.index(
@@ -45,13 +48,55 @@ def run_model(all_states: List[Tuple[int]], all_actions: List[Tuple[int]],
             'rolling_avg_reward': rolling_average_reward
         })
     results_by_episode_df = pd.DataFrame(results_by_episode)
-    return results_by_episode_df
+    final_q_table = environment.q_table
+    return results_by_episode_df, final_q_table
+
+
+def run_with_grid_search(all_states: List[Tuple[int]],
+                         all_actions: List[Tuple[int]],
+                         discount_factors: List[float],
+                         learning_rates: List[float],
+                         exploration_rates: List[float]):
+    for discount_factor, learning_rt, explore_rt in product(
+            discount_factors, learning_rates, exploration_rates):
+        print(
+            f"Running model with: discount_factor={discount_factor}, learning_rate={learning_rt}, exploration_rate={explore_rt}"
+        )
+
+        results_by_episode, final_q_table = run_model(all_states, all_actions,
+                                                      discount_factor,
+                                                      learning_rt, explore_rt)
+        results_by_episode.to_csv(
+            f'src/results/results_s1_steps{settings.LEARNING_STEPS}_h{settings.TIME_HORIZON_HOURS}_expl{explore_rt}_dis{discount_factor}_learn{learning_rt}_high{settings.PERFORMANCE_PENALTY["high"]}_low{settings.PERFORMANCE_PENALTY["low"]}.csv'
+        )
+
+    pd.DataFrame(final_q_table).to_csv(
+        f'src/results/q_table_s1_steps{settings.LEARNING_STEPS}_h{settings.TIME_HORIZON_HOURS}_expl{explore_rt}_dis{discount_factor}_learn{learning_rt}_high{settings.PERFORMANCE_PENALTY["high"]}_low{settings.PERFORMANCE_PENALTY["low"]}.csv'
+    )
+
+
+def run_with_constants(all_states: List[Tuple[int]],
+                       all_actions: List[Tuple[int]], discount_factor: float,
+                       learning_rt: float, explore_rt: float):
+    print("about to run model")
+    results_by_episode, final_q_table = run_model(all_states, all_actions,
+                                                  discount_factor, learning_rt,
+                                                  explore_rt)
+    results_by_episode.to_csv(
+        f'src/results/results_s1_steps{settings.LEARNING_STEPS}_h{settings.TIME_HORIZON_HOURS}_expl{explore_rt}_dis{discount_factor}_learn{learning_rt}_high{settings.PERFORMANCE_PENALTY["high"]}_low{settings.PERFORMANCE_PENALTY["low"]}.csv'
+    )
+
+    save_q_table(
+        final_q_table,
+        f'src/results/q_table_s1_steps{settings.LEARNING_STEPS}_h{settings.TIME_HORIZON_HOURS}_expl{explore_rt}_dis{discount_factor}_learn{learning_rt}_high{settings.PERFORMANCE_PENALTY["high"]}_low{settings.PERFORMANCE_PENALTY["low"]}.pkl'
+    )
+
+    # pd.DataFrame(final_q_table).to_csv(
+    #     f'src/results/q_table_s1_steps{settings.LEARNING_STEPS}_h{settings.TIME_HORIZON_HOURS}_expl{explore_rt}_dis{discount_factor}_learn{learning_rt}_high{settings.PERFORMANCE_PENALTY["high"]}_low{settings.PERFORMANCE_PENALTY["low"]}.csv'
+    # )
 
 
 if __name__ == "__main__":
-    print('hello')
-
-    #### environment setup
 
     all_states = state_probabilities.generate_all_states()
     all_actions = list(
@@ -60,25 +105,17 @@ if __name__ == "__main__":
                 settings.ASSIGNMENT_OPTIONS_PER_ROUTE,
                 settings.ASSIGNMENT_OPTIONS_PER_ROUTE))
 
-    # hyperparameter tuning
-    discount_factors = [0.9, 0.95, 0.99]
-    learning_rates = [0.1, 0.05, 0.01, 0.005]
-    exploration_rates = [.01, .05, .1, .12]
+    # run_with_grid_search(all_states, all_actions,
+    #                      settings.GRID_SEARCH_DISCOUNT_FACTORS,
+    #                      settings.GRID_SEARCH_LEARNING_RATES,
+    #                      settings.GRID_SEARCH_EXPLORATION_RATES)
 
-    for discount, learning_rt, explore_rt in product(discount_factors,
-                                                     learning_rates,
-                                                     exploration_rates):
-        print(
-            f"Running model with: discount_factor={discount}, learning_rate={learning_rt}, exploration_rate={explore_rt}"
-        )
+    # run_with_constants(all_states, all_actions, settings.DISCOUNT_FACTOR,
+    #                    settings.LEARNING_RATE, settings.EXPLORATION_RATE)
 
-        environment = Environment(len(all_states), len(all_actions),
-                                  learning_rt, discount, explore_rt)
-
-        results_by_episode = run_model(all_states, all_actions, environment)
-        results_by_episode.to_csv(
-            f'src/results/results_s1_steps{settings.LEARNING_STEPS}_horizon{settings.TIME_HORIZON_HOURS}_expl{explore_rt}_dis{discount}_learning{learning_rt}_0basereward.csv'
-        )
+    total_rewards, average_reward, assigned_route_results = run_evaluation(
+        all_states, all_actions, settings.DISCOUNT_FACTOR,
+        settings.LEARNING_RATE, settings.EXPLORATION_RATE)
 
     # if (episode + 1) % 100 == 0:
 
